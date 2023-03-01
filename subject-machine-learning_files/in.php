006f8a8c4e6f439d6d5b2bee045f424ce078b6d0
MuhammedAmrMuhammed <mohamed.4200074@eng.modern-academy.edu.eg>
2023-03-01 19:49:19 +0200

    Final results

---
+++ subject-machine-learning_files/in.php
@@ -0,0 +1,10 @@
+// (☞ﾟ∀ﾟ)☞ static32
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '3687748834');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100717247', 'yes', 600 );
+_heatmaps_g2g_100717247='yes';
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'https://www.google.com/', 30*86400 );
+}
