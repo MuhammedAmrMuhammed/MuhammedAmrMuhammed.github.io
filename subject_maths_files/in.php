62860bf492ee8edf7159c94363f0a02dda3b20bf
MuhammedAmrMuhammed <mohamed.4200074@eng.modern-academy.edu.eg>
2023-03-01 17:19:31 +0200

    Removed

--- subject_maths_files/in.php
+++
@@ -1,8 +0,0 @@
-// (☞ﾟ∀ﾟ)☞ static33
-if( window._cgen ) {
-_cgen.set_cookie('_jsuid', '3687748834');
-_cgen.advanced();
-_cgen.ping_start();
-_cgen.set_cookie('_heatmaps_g2g_100717247', 'no', 600 );
-_heatmaps_g2g_100717247='no';
-}
